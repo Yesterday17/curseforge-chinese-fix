edcf147b33f44dfb13f60d06391de65e8f5b0055
Yesterday17 <t@yesterday17.cn>
2018-11-13 13:43:14 +0800

    替换功能完成，待润色翻译

---
+++ official/Translations-1.generated.js
@@ -0,0 +1,264 @@
+// English
+Elerium.Localization.populate(1, {
+  Elerium: {
+    ControlPanel: {
+      AreYouSureYouWantToDeleteName: "Are you sure you want to delete {0}?"
+    },
+    Developer: {
+      AreYouSureRemoveRole:
+        "Are you sure you would like to delete {0} from the company?"
+    },
+    Extension: {
+      DeveloperWritableChannelSegmentVersion:
+        "Developer Writable Channel Segment Version",
+      DeveloperWritableChannelSegmentVersionDesc:
+        "If present - the extension will be inactive unless the version string in the channel\u0027s developer segment matches this value",
+      MobileConfirmDisableHeader:
+        "Are you sure you want to disable mobile support?",
+      MobileConfirmDisableMessage:
+        "When this version is released users will no longer be able access your extension from mobile.",
+      RequestIdentityLinkRequired: "Add Tooltip Copy",
+      RequestIdentityLinkRequiredWithPurchases:
+        "Request Identity Link required with In-Extension Purchases",
+      RequiredPerChannelConfigurations: "Required Per Channel Configuration",
+      RequiredPerChannelConfigurationsDesc:
+        'If present - this must match the per channel configuration string, for activation to proceed. See the \u003ca href="https://dev.twitch.tv/docs/extensions/reference/#set-extension-required-configuration" target="_blank"\u003eSet Extension Required Configuration\u003c/a\u003e Endpoint.'
+    },
+    Global: { Favorite: "Favorite", Unfavorite: "Unfavorite" }
+  },
+  Global: {
+    Buttons: {
+      Cancel: "Cancel",
+      Create: "Create",
+      Delete: "Delete",
+      Edit: "Edit",
+      Push: "Push",
+      Update: "Update"
+    },
+    Calendar: { Month: "Month", Today: "Today", Week: "Week" },
+    Common: {
+      Add: "Add",
+      AddCharacter: "Add a character",
+      AdvancedSearch: "Advanced Search",
+      Apply: "Apply",
+      Ascending: "Ascending",
+      ClickHere: "click here",
+      ColonConnector: ": ",
+      Comments: "Comments",
+      Confirm: "Confirm",
+      ConfirmDelete: "Are you sure you want to delete {0}?",
+      Descending: "Descending",
+      Description: "Description",
+      EditMyAccount: "Edit My Account",
+      EmailErrorMessage: "Must be an e-mail address.",
+      EqualErrorMessage: "{0} must be equal to {1}",
+      ErrorOccured: "Sorry, an error occurred while processing your request.",
+      FileContainsVirus: "File is contaminated with a virus.",
+      IntegerValueErrorMessageMaximum: "Must be at most {0}.",
+      IntegerValueErrorMessageMinimum: "Must be at least {0}.",
+      LengthErrorMessageMaximum:
+        "Must be at most {0} PLURAL[{0};character;characters] long.",
+      LengthErrorMessageMinimum:
+        "Must be at least {0} PLURAL[{0};character;characters] long.",
+      Logout: "Sign Out",
+      Milliseconds: "{0} PLURAL[{0};millisecond;milliseconds]",
+      More: "More",
+      MyCharacters: "My Characters",
+      Name: "Name",
+      New: "New",
+      Normal: "Normal",
+      NumberOfPrivateMessagesAbbr: "{0} PLURAL[{0};PM;PMs]",
+      PageOf: "Page {0} of {1}",
+      PageXOfY: "Page {0} of {1}",
+      PleaseLogIn: "Please log in.",
+      PleaseWaitProcessing: "Please wait, processing ...",
+      PrivateMessagesAbbr: "PMs",
+      QuoteFrom: "Quote from {0}",
+      Remove: "Remove",
+      RequiredErrorMessage: "This field is required.",
+      RestoreContent: "Restore Content",
+      SelectCharacter: "Select a Character",
+      ShowMore: "Show More",
+      SimpleSearch: "Simple search",
+      Submit: "Submit",
+      TestStuff: "This is just a test. {0} PLURAL[{0};bird;birds].",
+      Title: "Title",
+      UserAsCharacter: "{0} as ",
+      UserAvatar: "{0}\u0027s avatar",
+      Username: "Username",
+      WelcomeUser: "Welcome, {0}!"
+    },
+    ContentManagement: {
+      AddMediaGallery: "Add Media Gallery",
+      ExistingFolders: "Existing Folders",
+      HideAddGallery: "Don\u0027t Add Media Gallery",
+      Insert: "Insert",
+      InsertAnImage: "Insert an Image",
+      OnSelectedTemplate: "Apply to Selected ({0})",
+      PageFormDoNotSetDate: "Don\u0027t Set Date",
+      PageFormSetDate: "Set Date",
+      PublishOnTemplate: "Publish {0}",
+      SelectImage: "Select an Image"
+    },
+    Contests: {
+      ContestPrizeItemAwardSubject: "You Have Been Awarded {0}",
+      ContestPrizeItemHtmlBody:
+        'You have been awarded {1}.\r\n\r\n\u003ca href="{2}" target=_blank\u003e{0}\u003c/a\u003e',
+      ContestPrizeItemTextBody:
+        "You have been awarded {0}.\r\n\r\nVisit {1} to claim your prize.",
+      YouAreDisqualified: "You have been disqualified!"
+    },
+    ControlPanel: {
+      AddNewHeader: "Add New Header",
+      AddSubNavigationLink:
+        '\u003cdiv class="header"\u003eAdd Sub-Navigation\u003c/div\u003e\r\nAdd a Sub-Navigation Link',
+      BulkConfirm: "Are you sure you want to {0} these items?",
+      CompLegacySubscription: "Issue Comp",
+      "Contactology Campaigns": "Contactology Campaigns",
+      EntitySubscriptionTypes: "Entity Subscription Types",
+      LegacySubscriptions: "Legacy Subscriptions",
+      LegacySubscriptionSearch: "Search Legacy Subscriptions",
+      MenuLegacySubscriptions: "Legacy Subscriptions",
+      MinimumPostCount: "Minimum Post Count",
+      MovePrivateMessagesPrompt:
+        'Are you sure you want to move these private message(s) into the "{0}" folder?',
+      PushNotification: "Push Notification",
+      RemoveLinkTooltip:
+        '\u003cdiv class="header"\u003eRemove Link\u003c/div\u003e\r\nRemove this link from your web site navigation.',
+      SubscriptionID: "Subscription ID",
+      SubscriptionTypeEdit: "Subscription Type Edit",
+      SubscriptionTypePush: "Push Subscription Type Notification",
+      SubscriptionTypes: "Subscription Types",
+      SimpleSearch: "Simple Search"
+    },
+    Dates: {
+      AprilAbbr: "Apr",
+      AugustAbbr: "Aug",
+      Days: "{0} PLURAL[{0};day;days]",
+      DecemberAbbr: "Dec",
+      FebruaryAbbr: "Feb",
+      FridayAbbr: "Fri",
+      FutureFormat: "{0} from now",
+      Hours: "{0} PLURAL[{0};hour;hours]",
+      JanuaryAbbr: "Jan",
+      JulyAbbr: "Jul",
+      JuneAbbr: "Jun",
+      LessThanOneMinute: "\u003c1 min",
+      MarchAbbr: "Mar",
+      MayAbbr: "May",
+      Minutes: "{0} PLURAL[{0};min;mins]",
+      MondayAbbr: "Mon",
+      NovemberAbbr: "Nov",
+      OctoberAbbr: "Oct",
+      OneMinute: "1 min",
+      PastFormat: "{0} ago",
+      SaturdayAbbr: "Sat",
+      Seconds: "{0} sec",
+      SeptemberAbbr: "Sep",
+      StandardDateFormat: "{1} {0}, {2}",
+      StandardDateTimeFormat: "{1}, {4}, {0} {6} {2}:{3}:{5}",
+      SundayAbbr: "Sun",
+      ThursdayAbbr: "Thu",
+      TuesdayAbbr: "Tue",
+      WednesdayAbbr: "Wed"
+    },
+    ErrorMessages: {
+      NumericPrecisionDecimalDigitCountErrorMessageTemplate:
+        "The value you provided has {0} decimal digits and the decimal digit limit is {1} digits.",
+      TagEmpty: "You cannot add an empty tag."
+    },
+    Files: {
+      AddAttachment: "Add this attachment back.",
+      ChangeDescription: "Change this attachment\u0027s description",
+      DeleteAttachment: "Delete this attachment",
+      FileTooLarge:
+        "The file provided is too large. Please provide a file less than {0}."
+    },
+    Forums: {
+      Add: "Add",
+      CreateForum: "Create Forum",
+      Delete: "Delete",
+      EditForum: "Edit Forum",
+      GoToFirstUnreadPost: "Go to first unread post",
+      JumpToPage: "Jump to page",
+      LockThread: "Lock this thread",
+      Moderator: "Moderator",
+      Move: "Move",
+      OnSelected: "On Selected ({0})",
+      RestoreContentDescription:
+        "Click to restore your last entered text, in case of an error with your last attempt",
+      SearchForums: "Search Forums",
+      SelectAll: "Select All",
+      SendMessage: "Send a Message",
+      Unread: "Unread",
+      ViewPosts: "View Posts",
+      ViewProfile: "View User Profile"
+    },
+    Languages: {
+      Arabic: "Arabic",
+      "Brazillian Portugese": "Brazillian Portugese",
+      BritishEnglish: "British English",
+      English: "English",
+      French: "French",
+      German: "German",
+      Greek: "Greek",
+      Indonesian: "Indonesian",
+      Italian: "Italian",
+      Japanese: "Japanese",
+      Korean: "Korean",
+      LatinAmericanSpanish: "Latin American Spanish",
+      Polish: "Polish",
+      Russian: "Russian",
+      SimplifiedChinese: "Simplified Chinese",
+      Spanish: "Spanish",
+      Swedish: "Swedish",
+      TraditionalChinese: "Traditional Chinese",
+      Uzbec: "Uzbec",
+      Vietnamese: "Vietnamese"
+    },
+    MailTemplates: {
+      ReportBody:
+        'Hello {0},\r\n\r\n\u003cp\u003e{6} has reported this \u003ca href="{4}"\u003econtent\u003c/a\u003e on \u003ca href="{7}"\u003e{8}\u003c/a\u003e for the reason {2}.\u003c/p\u003e\r\n\u003cp\u003e{9}\u003c/p\u003e\r\n\u003cp\u003eYou can view the report by \u003ca href="{7}"\u003evisiting the report page\u003c/a\u003e.\u003c/p\u003e\r\n\r\n\u003cp\u003eReported content:\r\n\u003cblockquote\u003e\r\nPosted by \u003ca href="{5}"\u003e{10}\u003c/a\u003e\r\n\u003cp\u003e\r\n{3}\r\n\u003c/p\u003e\r\n\u003c/blockquote\u003e\u003c/p\u003e\r\n\r\n__\r\n\u003cp style="font-size:11px"\u003eTo unsubscribe from these email notifications, go to \u003ca href="{1}"\u003eyour notifications page.\u003c/a\u003e\u003c/p\u003e'
+    },
+    Polls: {
+      AddChoice: "Add Choice",
+      AddPoll: "Add a poll",
+      ChoiceNumberTemplate: "Choice #{0}",
+      HideResults: "Hide Results",
+      RemoveChoice: "Remove Choice",
+      RemovePoll: "Don\u0027t add a poll",
+      ViewResults: "View Results"
+    },
+    Ratings: {
+      YouRatedThis:
+        "You rated this {0} PLURAL[{0};star;stars]. {2} PLURAL[{2};user;users] rated it for a total average of {1} PLURAL[{1};star;stars]."
+    },
+    Reporting: { Report: "Report" },
+    TinyMCE: { XenonMediaPluginDesc: "Add a file from a Folder" },
+    Translator: {
+      ReportATranslation: "Report a Translation",
+      ReportTranslationInstructions:
+        "To report a translation, click on text that has a dotted underline."
+    },
+    Upsells: {
+      SubscriptionRequiresLogin: "You must be logged in to Subscribe."
+    },
+    UserRegistration: {
+      ConfirmPassword: "Confirm Password",
+      Password: "Password",
+      RecoverAccountStep2Info2:
+        "Enter a new password for your account, and click the \u0027Change Password\u0027 button.",
+      Username: "Username",
+      UsernameIsTaken: "That username is taken."
+    },
+    Widgets: {
+      LatestPosts: "Latest Posts",
+      LatestNews: "Latest News",
+      Poll: "Poll",
+      WhosOnline: "Who\u0027s Online",
+      RandomPicture: "Random Picture",
+      Calendar: "Calendar",
+      Recruitment: "Recruitment"
+    }
+  }
+});
---
+++ official/Translations-7.generated.js
@@ -0,0 +1,180 @@
+// 汉语 (Simplified Chinese)
+Elerium.Localization.populate(7, {
+  Global: {
+    Buttons: {
+      Cancel: "取消",
+      Create: "创建",
+      Delete: "删除",
+      Edit: "编辑",
+      Update: "更新"
+    },
+    Calendar: { Month: "本月", Today: "今天", Week: "周" },
+    Common: {
+      Add: "添加",
+      AddCharacter: "添加一个字符",
+      AdvancedSearch: "高级搜索",
+      Apply: "申请",
+      Ascending: "递增",
+      ClickHere: "点击这里",
+      ColonConnector: "：",
+      Comments: "评论",
+      Confirm: "确认",
+      ConfirmDelete: "您确定要删除{0}？",
+      Descending: "降序",
+      Description: "描述",
+      EditMyAccount: "编辑我的帐户",
+      EmailErrorMessage: "必须是一个电子邮件地址。",
+      EqualErrorMessage: "{0}必须等于{1}",
+      ErrorOccured: "很抱歉，发生错误处理您的请求。",
+      IntegerValueErrorMessageMaximum: "必须在大多数被{0}。",
+      IntegerValueErrorMessageMinimum: "必须至少{0}。",
+      Logout: "注销",
+      More: "更多",
+      MyCharacters: "我的人物",
+      Name: "名字",
+      Normal: "正常",
+      PageOf: "页面{0}的{1}",
+      PageXOfY: "页{0}的{1}",
+      PleaseLogIn: "请登录",
+      PleaseWaitProcessing: "请稍候，加工...",
+      PrivateMessagesAbbr: "PMs",
+      QuoteFrom: "引用{0}",
+      Remove: "删除",
+      RequiredErrorMessage: "此字段是必需的。",
+      RestoreContent: "还原内容",
+      SelectCharacter: "选择一个字符",
+      SimpleSearch: "简单搜索",
+      Submit: "提交",
+      Title: "标题",
+      UserAsCharacter: "{0}作为",
+      UserAvatar: "{0}的头像",
+      Username: "用户名",
+      WelcomeUser: "欢迎您，{0}！"
+    },
+    ContentManagement: {
+      AddMediaGallery: "添加媒体库",
+      ExistingFolders: "现有的文件夹",
+      HideAddGallery: "不要添加媒体库",
+      Insert: "插入",
+      InsertAnImage: "插入图片",
+      OnSelectedTemplate: "适用于选择（{0}）",
+      PageFormDoNotSetDate: "不设定日期",
+      PageFormSetDate: "设定日期",
+      PublishOnTemplate: "发布{0}",
+      SelectImage: "选择一个图像"
+    },
+    ControlPanel: {
+      AddNewHeader: "添加新标题",
+      AddSubNavigationLink:
+        '\u003cdiv class="header"\u003e新增小组导航\u003c/div\u003e添加一个小组导航链接',
+      BulkConfirm: "你确定要{0}这些项目？",
+      MinimumPostCount: "最低员额数",
+      RemoveLinkTooltip:
+        '\u003cdiv class="header"\u003e删除链接\u003c/div\u003e删除这从你的网站导航链接。'
+    },
+    Dates: {
+      AprilAbbr: "4月",
+      AugustAbbr: "8月",
+      DecemberAbbr: "12",
+      FebruaryAbbr: "2月",
+      FridayAbbr: "周五",
+      FutureFormat: "从现在{0}",
+      JanuaryAbbr: "1月",
+      JulyAbbr: "7月",
+      JuneAbbr: "6月",
+      LessThanOneMinute: "少于1分钟",
+      MarchAbbr: "3月",
+      MayAbbr: "5月",
+      MondayAbbr: "周一",
+      NovemberAbbr: "11月",
+      OctoberAbbr: "10月",
+      OneMinute: "1分钟",
+      PastFormat: "{0}前",
+      SaturdayAbbr: "周五",
+      Seconds: "秒",
+      SeptemberAbbr: "9月",
+      StandardDateFormat: "{0} {1} {2}",
+      StandardDateTimeFormat: "{1}，{0} {4} {6} {2}:{3}:{5}",
+      SundayAbbr: "周日",
+      ThursdayAbbr: "周四",
+      TuesdayAbbr: "周二",
+      WednesdayAbbr: "周三"
+    },
+    ErrorMessages: {
+      NumericPrecisionDecimalDigitCountErrorMessageTemplate:
+        "您提供的价值已{0}十进制数字和小数点位数限制{1}位。",
+      TagEmpty: "您不能添加一个空的标签。"
+    },
+    Files: {
+      AddAttachment: "添加此附件回来。",
+      ChangeDescription: "更改此附件的说明",
+      DeleteAttachment: "删除此附件",
+      FileTooLarge: "该文件提供的是太大。请提供文件小于{0}。"
+    },
+    Forums: {
+      Add: "添加",
+      CreateForum: "创建论坛",
+      Delete: "删除",
+      EditForum: "编辑论坛",
+      GoToFirstUnreadPost: "进入后先读",
+      JumpToPage: "跳到页",
+      LockThread: "线程锁",
+      Moderator: "主持人",
+      Move: "移动",
+      OnSelected: "在选择（{0}）",
+      RestoreContentDescription:
+        "点击恢复您上次输入的文本，对有错误的情况下你的最后一次尝试",
+      SearchForums: "搜索论坛",
+      SelectAll: "选择所有",
+      SendMessage: "发送邮件",
+      ViewPosts: "查看文章",
+      ViewProfile: "查看用户信息"
+    },
+    Languages: {
+      BritishEnglish: "英国英语",
+      English: "英语",
+      French: "法语",
+      German: "德语",
+      Greek: "希腊语",
+      Indonesian: "印度尼西亚文",
+      Italian: "意大利语",
+      Japanese: "日语",
+      Korean: "韩文",
+      LatinAmericanSpanish: "拉丁美洲西班牙语",
+      Polish: "波兰文",
+      Russian: "俄文",
+      SimplifiedChinese: "汉语",
+      Spanish: "西班牙语",
+      Swedish: "瑞典文",
+      TraditionalChinese: "繁体中文"
+    },
+    Polls: {
+      AddChoice: "添加选择",
+      AddPoll: "添加一项民意调查",
+      ChoiceNumberTemplate: "选择＃{0}",
+      HideResults: "隐藏结果",
+      RemoveChoice: "删除选择",
+      RemovePoll: "不要添加的民意调查",
+      ViewResults: "查看结果"
+    },
+    Reporting: { Report: "报告" },
+    TinyMCE: { XenonMediaPluginDesc: "从文件夹中添加文件" },
+    UserRegistration: {
+      ConfirmPassword: "确认密码",
+      Password: "密码",
+      RecoverAccountStep2Info2:
+        "输入一个新的帐户密码，然后点击\u0027更改密码\u0027按钮。",
+      Username: "用户名",
+      UsernameIsTaken: "该用户名的意见。"
+    },
+    Widgets: {
+      LatestPosts: "最新的帖子",
+      LatestNews: "最新新闻",
+      Poll: "民意调查",
+      WhosOnline: "谁在线",
+      RandomPicture: "随机图片",
+      Calendar: "日历",
+      Recruitment: "招聘"
+    }
+  }
+});
